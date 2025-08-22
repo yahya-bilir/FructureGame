cff91d5a44ce40749757ab263976460d589de877
yahya-bilir <hostmailler00@gmail.com>
2025-08-23 01:30:08 +0300

    Update PropertyQuery.cs

--- Assets/_Main/Project/Scripts/Gameplay/PropertySystem/PropertyQuery.cs
+++ Assets/_Main/Project/Scripts/Gameplay/PropertySystem/PropertyQuery.cs
@@ -2,28 +2,18 @@
 {
     public enum PropertyQuery
     {
-        Health,
-        MaxHealth,
-        Damage,
-        Armor,
-        Gold,
-        CritDamage,
-        CritChance,
-        ElectricChance,
-        ElectricDamage,
-        DaggerChance,
-        DaggerDamage,
-        PoisonChance,
-        PoisonDamage,
-        WeaknessChance,
-        WeaknessDamage,
-        RootsChance,
-        RootsDamage,
-        LifeSteal,
-        IceChance,
-        IceDamage,
-        Speed,
-        AttackRange,
-        AttackSpeed
+        Health = 0,
+        MaxHealth = 1,
+        Damage = 2,
+        Armor = 3,
+        Gold = 4,
+        CritDamage = 5,
+        CritChance = 6,
+        LifeSteal = 17,
+        IceChance = 18,
+        IceDamage = 19,
+        Speed = 20,
+        AttackRange = 21,
+        AttackSpeed = 22
     }
 }
